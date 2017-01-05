a08718fe01e78204e840b9203929851fef77050d
Alvin Oktavianus <agentsam1995@gmail.com>
2017-01-05 21:27:34 +0700

    Create holiday with layout and logic

--- application/models/MasterData.php
+++ application/models/MasterData.php
@@ -3,27 +3,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class MasterData extends CI_Model {
 
-    public function get_all_department()
-    {
-        return $this->db->get('MsDepartment')->result();
-    }
+    /** 
+     * Fungsi untuk insert data
+     */
 
     public function insert_new_department($data)
     {
-        $this->db->where('IsActive', 'Y');
         $this->db->insert('MsDepartment', $data);
-    }
-
-    public function get_all_jobposition()
-    {
-        return $this->db->get('MsJobPosition')->result();
     }
 
     public function insert_new_jobposition($data)
     {
-        $this->db->where('IsActive', 'Y');
         $this->db->insert('MsJobPosition', $data);
     }
+
+    public function insert_new_holiday($data)
+    {
+        $this->db->insert('MsHoliday', $data);
+    }
+
+
+    /** 
+     * Fungsi untuk update data
+     */
 
     public function update_department($id, $data)
     {
@@ -35,6 +37,32 @@ class MasterData extends CI_Model {
     {
         $this->db->where('JobPositionID', $id);
         $this->db->update('MsJobPosition', $data);
+    }
+
+    public function update_holiday($id, $data)
+    {
+        $this->db->where('HolidayId', $id);
+        $this->db->update('MsHoliday', $data);
+    }
+
+
+    /** 
+     * Fungsi untuk select data
+     */
+
+    public function get_all_department()
+    {
+        return $this->db->get('MsDepartment')->result();
+    }
+
+    public function get_all_jobposition()
+    {
+        return $this->db->get('MsJobPosition')->result();
+    }
+
+    public function get_all_holiday()
+    {
+        return $this->db->get('MsHoliday')->result();
     }
 
 }
--- application/views/layouts/holiday/holiday.php
+++ application/views/layouts/holiday/holiday.php
@@ -57,11 +57,11 @@
                         </div>
 
                         <div class="form-group row">
-                            <?php echo form_label('End Name', 'enddate', array( 'class' => 'col-sm-4 form-control-label' )); ?>
+                            <?php echo form_label('End Date', 'enddate', array( 'class' => 'col-sm-4 form-control-label' )); ?>
                             <div class="col-sm-8">
                                 <?php
                                     $data = array(
-                                        'type' => 'text',
+                                        'type' => 'date',
                                         'class' => 'form-control',
                                         'id' => 'enddate',
                                         'name' => 'enddate',
@@ -101,7 +101,7 @@
 
             </div>
 
-<!--             <?php if (count($holidays) > 0): ?>
+            <?php if (count($holidays) > 0): ?>
                 <div class="row">
                     <div class="col-md-8">
                         <div class="table-responsive">
@@ -109,7 +109,10 @@
                                 <thead class="thead-inverse">
                                     <tr>
                                         <th>#</th>
-                                        <th>Job Position Name</th>
+                                        <th>Holiday Name</th>
+                                        <th>Start Date</th>
+                                        <th>End Date</th>
+                                        <th>Holiday Type</th>
                                         <th></th>
                                     </tr>
                                 </thead>
@@ -119,15 +122,22 @@
 
                                         <tr>
                                             <td> <?php echo $holiday->HolidayId; ?> </td>
-                                            <td> <?php echo $holiday->Description ?> </td>
+                                            <td> <?php echo $holiday->Description; ?> </td>
+                                            <td> <?php echo $holiday->StartDate; ?> </td>
+                                            <td> <?php echo $holiday->EndDate; ?> </td>
+                                            <td>
+                                                <?php if ( $holiday->HolidayType == 'N' ): ?>
+                                                    Libur Nasional
+                                                <?php else: ?>
+                                                    Cuti Bersama
+                                                <?php endif; ?>
+                                            </td>
                                             <td>
                                                 <?php if ( $holiday->IsActive == 'Y' ): ?>
                                                     <a href="<?php echo base_url(); ?>holiday/deactivate?id=<?php echo $holiday->HolidayId; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you?')">Deactivate</a>
                                                 <?php else: ?>
                                                     <a href="<?php echo base_url(); ?>holiday/activate?id=<?php echo $holiday->HolidayId; ?>" class="btn btn-success btn-sm">Activate</a>
                                                 <?php endif; ?>
-                                                <br>
-                                                <a href="<?php echo base_url(); ?>holiday/update?id=<?php echo $holiday->HolidayId; ?>" class="btn btn-info btn-sm">Update</a>
                                             </td>
                                         </tr>
 
@@ -138,7 +148,7 @@
                         </div>                        
                     </div>
                 </div>
-            <?php endif; ?> -->
+            <?php endif; ?>
 
         </div>
     </section>
