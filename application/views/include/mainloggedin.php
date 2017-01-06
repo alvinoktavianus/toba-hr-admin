223a2ccf4abcd8a820c784952ec823f7b5d96885
Alvin Oktavianus <agentsam1995@gmail.com>
2017-01-06 23:06:37 +0700

    Update ManageEmployee UI

--- application/views/include/mainloggedin.php
+++ application/views/include/mainloggedin.php
@@ -48,12 +48,8 @@
                             <li class="<?php if ($page == 'home') { echo "active"; } ?>">
                                 <a href="<?php echo base_url(); ?>"> <i class="fa fa-home"></i> Dashboard </a>
                             </li>
-                            <li class="<?php if ($page == 'manageemployee' || $page == 'addemployee') { echo "active open"; } ?>">
-                                <a href="#"> <i class="fa fa-users" aria-hidden="true"></i> Manage Employee <i class="fa arrow"></i> </a>
-                                <ul>
-                                    <li class="<?php if ($page == 'manageemployee') { echo "active"; } ?>"> <a href="<?php echo base_url(); ?>manageemployee">View Employee</a> </li>
-                                    <li class="<?php if ($page == 'addemployee') { echo "active"; } ?>"> <a href="<?php echo base_url(); ?>manageemployee/add">Add New Employee</a> </li>
-                                </ul>
+                            <li class="<?php if ($page == 'manageemployee' || $page == 'addemployee') { echo "active"; } ?>">
+                                <a href="<?php echo base_url(); ?>manageemployee"> <i class="fa fa-users" aria-hidden="true"></i> Manage Employee </a>
                             </li>
                             <li class="<?php if ($page == 'holidayschedule' || $page == 'addholidayschedule') { echo "active open"; } ?>">
                                 <a href="#"> <i class="fa fa-credit-card" aria-hidden="true"></i> Schedule <i class="fa arrow"></i> </a>
--- application/views/layouts/employee/manageemployee.php
+++ application/views/layouts/employee/manageemployee.php
@@ -6,45 +6,63 @@
 
         <div class="card card-block">
 
-            <div class="table-responsive">
-                <table class="table table-striped table-bordered table-sm">
-                    <thead class="thead-inverse">
-                        <tr>
-                            <th>Employee ID</th>
-                            <th>Full Name</th>
-                            <th>Address</th>
-                            <th>Personal Leave Balance</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
+            <div class="row">
+                
+                <div class="col-md-8">
+                    <?php if ($this->session->flashdata('success')): ?>
+                        <div class="alert alert-success" role="alert">
+                            <strong><?php echo $this->session->flashdata('success'); ?></strong>
+                        </div>
+                    <?php endif; ?>                    
+                </div>
 
-                    <?php foreach( $employees as $employee ): ?>
+                <div class="col-md-4">
+                    <a href="<?php echo base_url(); ?>manageemployee/add" class="btn btn-block btn-info">Add</a>
+                </div>
 
-                        <tr>
-                            <td> <?php echo $employee->EmployeeID; ?> </td>
-                            <td> <?php echo $employee->FullName ?> </td>
-                            <td> <?php echo $employee->Address ?> </td>
-                            <td> <?php echo $employee->PersonalLeaveBalance ?> </td>
-                            <td>
-                                <a href="<?php echo base_url(); ?>manageemployee/update?id=<?php echo $employee->EmployeeID; ?>" class="btn btn-info btn-sm">Update</a>
-                                <br>
-                                <?php if ( $employee->IsActive == 'Y' ): ?>
-                                    <a href="<?php echo base_url(); ?>manageemployee/deactivate?id=<?php echo $employee->EmployeeID; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you?')">Deactivate</a>
-                                <?php else: ?>
-                                    <a href="<?php echo base_url(); ?>manageemployee/activate?id=<?php echo $employee->EmployeeID; ?>" class="btn btn-success btn-sm">Activate</a>
-                                <?php endif; ?>
-                                <br>
-                                <?php if ( $employee->IsEmployee == 'Y' ): ?>
-                                    <a href="<?php echo base_url(); ?>manageemployee/remove?id=<?php echo $employee->EmployeeID; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you?')">Remove</a>
-                                <?php endif; ?>
-                            </td>
-                        </tr>
+            </div>
+            
+            <div class="row">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered table-sm">
+                        <thead class="thead-inverse">
+                            <tr>
+                                <th>Employee ID</th>
+                                <th>Full Name</th>
+                                <th>Address</th>
+                                <th>Personal Leave Balance</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
-                    <?php endforeach; ?>
+                        <?php foreach( $employees as $employee ): ?>
 
-                    </tbody>
-                </table>
+                            <tr>
+                                <td> <?php echo $employee->EmployeeID; ?> </td>
+                                <td> <?php echo $employee->FullName ?> </td>
+                                <td> <?php echo $employee->Address ?> </td>
+                                <td> <?php echo $employee->PersonalLeaveBalance ?> </td>
+                                <td>
+                                    <a href="<?php echo base_url(); ?>manageemployee/update?id=<?php echo $employee->EmployeeID; ?>" class="btn btn-info btn-sm">Update</a>
+                                    <br>
+                                    <?php if ( $employee->IsActive == 'Y' ): ?>
+                                        <a href="<?php echo base_url(); ?>manageemployee/deactivate?id=<?php echo $employee->EmployeeID; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you?')">Deactivate</a>
+                                    <?php else: ?>
+                                        <a href="<?php echo base_url(); ?>manageemployee/activate?id=<?php echo $employee->EmployeeID; ?>" class="btn btn-success btn-sm">Activate</a>
+                                    <?php endif; ?>
+                                    <br>
+                                    <?php if ( $employee->IsEmployee == 'Y' ): ?>
+                                        <a href="<?php echo base_url(); ?>manageemployee/remove?id=<?php echo $employee->EmployeeID; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you?')">Remove</a>
+                                    <?php endif; ?>
+                                </td>
+                            </tr>
+
+                        <?php endforeach; ?>
+
+                        </tbody>
+                    </table>
+                </div>
             </div>
 
         </div>
