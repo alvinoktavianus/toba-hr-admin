de02d597261a5c2c0009a4662c67c81ecd0b147c
Alvin Oktavianus <agentsam1995@gmail.com>
2017-01-22 21:24:14 +0700

    Create dummy home UI

--- application/controllers/TobaHRM.php
+++ application/controllers/TobaHRM.php
@@ -10,6 +10,51 @@ class TobaHRM extends CI_Controller {
             $data['page_title'] = "Toba HRM Admin";
             $this->load->view('include/login', $data);
         } else {
+
+            $present = array();
+
+            $temp = array(
+                'line' => 1,
+                'name' => 'Alvin Oktavianus',
+                'checkin' => '09:00',
+                'checkout' => '-',
+                'location' => 'Jl. H. Kelik Srengseng, Kembangan',
+            );
+            array_push($present, $temp);
+
+            $temp = array(
+                'line' => 2,
+                'name' => 'Albert Kurniadinata',
+                'checkin' => '08:53',
+                'checkout' => '-',
+                'location' => 'Jl. H. Kelik Srengseng, Kembangan',
+            );
+            array_push($present, $temp);
+
+            $temp = array(
+                'line' => 3,
+                'name' => 'Anindia Rifara',
+                'checkin' => '09:10',
+                'checkout' => '-',
+                'location' => 'Jl. H. Kelik Srengseng, Kembangan',
+            );
+            array_push($present, $temp);
+
+            $absence = array();
+
+            $temp = array(
+                'line' => 1,
+                'name' => "Stephan Christianus",
+                'leavestart' => "23 Jan 2017",
+                'leaveend' => "23 Jan 2017",
+                'duration' => 1,
+                'reason' => "Urusan keluarga"
+            );
+            array_push($absence, $temp);
+
+            $data['presents'] = $present;
+            $data['absences'] = $absence;
+            
             $data['page_title'] = "Home | Toba HRM";
             $data['page'] = 'home';
             $this->load->view('include/mainloggedin', $data);
--- application/views/layouts/home.php
+++ application/views/layouts/home.php
@@ -4,10 +4,90 @@
     </div>
     <section class="section">
 
-        <div class="card card-block">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card card-block">
+                    <h5>Employee Absence</h5>
 
-            <h3>Welcome...</h3>
+<?php if (count($presents) > 0): ?>
 
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered table-sm">
+                <thead class="thead-inverse">
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Check In</th>
+                        <th>Check Out</th>
+                        <th>Location</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($presents as $present): ?>
+                        <tr>
+                            <td><?php echo $present['line']; ?></td>
+                            <td><?php echo $present['name']; ?></td>
+                            <td><?php echo $present['checkin']; ?></td>
+                            <td><?php echo $present['checkout']; ?></td>
+                            <td><?php echo $present['location']; ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+
+<?php else: ?>
+    No data
+<?php endif; ?>
+
+                </div>
+                
+            </div>
+            <div class="col-sm-6">
+                <div class="card card-block">
+                    <h5>Employee Leave</h5>
+
+<?php if (count($absences) > 0): ?>
+
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered table-sm">
+                <thead class="thead-inverse">
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Reason</th>
+                        <th>Leave day</th>
+                        <th>Leave duration</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($absences as $absence): ?>
+                        <tr>
+                            <td><?php echo $absence['line']; ?></td>
+                            <td><?php echo $absence['name']; ?></td>
+                            <td><?php echo $absence['reason']; ?></td>
+                            <td>
+<?php
+    if ($absence['duration'] > 1) {
+        echo $absence['leavestart']." - ".$absence['leaveend'];
+    } else {
+        echo $absence['leaveend'];
+    }
+?>
+                            </td>
+                            <td><?php echo $absence['duration']; ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+
+<?php else: ?>
+    No data
+<?php endif; ?>
+
+                </div>
+            </div>
         </div>
     </section>
 </article>
